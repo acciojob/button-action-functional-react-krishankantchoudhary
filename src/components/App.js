8aeeb69f28f7942e99c5f86d530ca92683e69b0b
krishan kant choudhary <83417448+krishankantchoudhary@users.noreply.github.com>
2025-12-19 18:40:49 +0530

    gg

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
+import Dis from "./Dis";
 
 function App() {
+  const[display,setDisplay]=useState("");
+
+  const handleClick=()=>{
+    setDisplay(<Dis/>)
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={handleClick}>Click</button>
+      <div>{display}</div>
     </div>
   );
 }
-
-
 export default App;
---
+++ src/components/Dis.js
@@ -0,0 +1,11 @@
+import React from "react";
+const Dis=()=>{
+    return(
+        
+            <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.</p>
+        
+        
+    )
+}
+
+export default Dis;
